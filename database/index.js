fe2721f4174b4dce351fe7715885d65c7599a2c5
afrodynamic <33985296+afrodynamic@users.noreply.github.com>
2023-04-24 09:51:20 -0400

    feat: complete mongoose repoSchema

--- database/index.js
+++ database/index.js
@@ -2,15 +2,31 @@ const mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/fetcher');
 
 let repoSchema = mongoose.Schema({
-  // TODO: your schema here!
+  name: String,
+  owner: {
+    login: String,
+    id: Number,
+    avatar_url: String,
+    url: String,
+    type: String
+  },
+  html_url: String,
+  description: String,
+  fork: Boolean,
+  forks_count: Number,
+  stargazers_count: Number,
+  watchers_count: Number,
+  open_issues_count: Number
 });
 
 let Repo = mongoose.model('Repo', repoSchema);
 
-let save = (/* TODO */) => {
-  // TODO: Your code here
-  // This function should save a repo or repos to
-  // the MongoDB
-}
+let save = (repos) => {
+  Repo.insertMany(repos)
+    .then(() => console.log('Repos saved to database'))
+    .catch((error) => {
+      console.error(error);
+    });
+};
 
 module.exports.save = save;
